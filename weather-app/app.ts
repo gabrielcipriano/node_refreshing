bb506f47b8df2497409ee16660aba3df650ccd8a
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2021-05-25 23:56:39 -0300

    secret config and initial api tests

--- weather-app/app.ts
+++ weather-app/app.ts
@@ -1,13 +1,22 @@
 import fetch from 'node-fetch';
 import * as fs from 'fs';
+
 // Getting apikey from secret.json with shape { "weatherApikey": "XXXXX", "geoApikey": "XXXXXX" }
 const secretJSON = fs.readFileSync('secret.json').toString();
-const { weatherApikey, geoApikey } = JSON.parse(secretJSON);
+const {weatherApikey, geoApikey} = JSON.parse(secretJSON);
+
+
+
 var query = 'Vitoria, ES';
 console.log(geoApikey);
+
+
 var url = `http://api.weatherstack.com/current?access_key=${weatherApikey}&query=${query}`;
+
+
 var response = fetch(url)
     .then(res => res.json())
     .then(json => json.current)
     .then(c => console.log(`It's currently ${c.temperature} degrees out. There is a ${c.precip}% chance of rain.`));
+
 // // var geo
